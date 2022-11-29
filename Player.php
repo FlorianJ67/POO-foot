680a6459d1bc8285a1520de332dd918eb555066f
Florian JACOB <florian.jacob96@gmail.com>
2022-11-29 11:37:42 +0100

    fini

--- Player.php
+++ Player.php
@@ -1,29 +1,28 @@
 <?php
 class Player {
-    protected $_name;
     protected $_fName;
+    protected $_name;
     protected $_sexe;
     protected $_birthday;
     protected $_nationality;
     protected $_contract;
         
     public function __construct(string $fName,string $name,string $sexe,$birthday,Nationality $nationality){
-        $this->_name = $name;
         $this->_fName = $fName;
+        $this->_name = $name;
         $this->_sexe = $sexe;
         $this->_birthday = $birthday;
         $this->_nationality = $nationality;
         $this->_nationality->addPlayer($this);
         $this->_contract = [];
-
     }
             
     //GET
-    public function getName(){
-        return $this->_name;
-    }
     public function getFName(){
         return $this->_fName;
+    }
+    public function getName(){
+        return $this->_name;
     }
     public function getsexe(){
         return $this->_sexe;
@@ -42,11 +41,11 @@ class Player {
     }
         
     //SET
-    public function setName($name){
-        $this->_name = $name;
-    }
     public function setFName($fName){
         $this->_fName = $fName;
+    }
+    public function setName($name){
+        $this->_name = $name;
     }
     public function setSexe($sexe){
         $this->_sexe = $sexe;
@@ -60,6 +59,12 @@ class Player {
 
     public function addToContract($newPlayer){
         array_push($this->_contract,$newPlayer);
+    }
+    public function getContractInfo(){
+        echo $this->getFName()." ".$this->getName()." :<br>";
+        foreach($this->_contract as $contract){
+            echo "A rejoint l'équipe du ".$contract->getTeam()->getName()." en ".$contract->getDate()."<br>";
+        } 
     }
 }
 ?>
--- index.php
+++ index.php
@@ -19,12 +19,18 @@ $pays1->listTeam();
 $nationality1 = New Nationality("Brésilien");
 
 $player1 = New Player("Neymar","Jr","Homme","1992-02-05",$nationality1);
-$player2 = New Player("Neym0","Senior","Homme","1942-08-12",$nationality1);
+$player2 = New Player("Neym0","Senior","Homme","1967-08-12",$nationality1);
 
 $c1 = New Contract($team1,$player1,"2005");
+$c1 = New Contract($team2,$player1,"2010");
+$c1 = New Contract($team3,$player1,"2019");
 $c2 = New Contract($team1,$player2,"2008");
 
 echo "<br>";
 $team1->listPlayers();
+
+
+echo "<br>";
+$player1->getContractInfo();
 
 ?>
